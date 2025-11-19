2c9e0c592638abb92213dd7a06f2c436d220b4c7
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2025-11-19 15:53:18 +0500

    Add todo CRUD apis

---
+++ app/Http/Controllers/API/TodoController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use Throwable;
+
+class TodoController extends Controller
+{
+    public function all(Request $request)
+    {
+        try {
+            $todos = Todo::where('user_id', $request->user()->id)->get();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'All todos fetched successfully',
+                'data' => $todos,
+            ]);
+        } catch (Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Something went wrong',
+                'data' => null,
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function get(Request $request, $id)
+    {
+        try {
+            $todo = Todo::where('id', $id)->where('user_id', $request->user()->id)->first();
+
+            if (!$todo) {
+                return response()->json(['message' => 'Todo not found or unauthorized'], 404);
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Todo with id ' . $id . ' fetched successfully',
+                'data' => $todo,
+            ]);
+        } catch (Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Something went wrong',
+                'data' => null,
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function create(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:100',
+            'description' => 'nullable|string|max:500',
+            'is_completed' => 'sometimes|boolean',
+        ]);
+
+        try {
+            $todo = Todo::create([
+                'user_id' => $request->user()->id,
+                'title' => $validated['title'],
+                'description' => $validated['description'] ?? null,
+                'is_completed' => $validated['is_completed'] ?? false,
+            ], 201);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Todo created successfully',
+                'data' => $todo,
+            ]);
+        } catch (Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Something went wrong',
+                'data' => null,
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $todo = Todo::where('id', $id)->where('user_id', $request->user()->id)->first();
+
+        if (!$todo) {
+            return response()->json(['message' => 'Todo not found or unauthorized'], 404);
+        }
+
+        $validated = $request->validate([
+            'title' => 'required|string|max:100',
+            'description' => 'nullable|string|max:500',
+            'is_completed' => 'sometimes|boolean',
+        ]);
+
+        try {
+            $todo->update($validated);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Todo updated successfully',
+                'data' => $todo,
+            ]);
+        } catch (Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Something went wrong',
+                'data' => null,
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $todo = Todo::where('id', $id)->where('user_id', $request->user()->id)->first();
+
+        if (!$todo) {
+            return response()->json(['message' => 'Todo not found or unauthorized'], 404);
+        }
+
+        try {
+            $todo->delete();
+            return response()->json([
+                'success' => true,
+                'message' => 'Todo deleted successfully'
+            ]);
+        } catch (Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Something went wrong',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +17,11 @@ Route::prefix('v1')->group(function () {
         Route::get('/profile', [AuthController::class, 'profile']);
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
+
+        Route::get('/todos', [TodoController::class, 'all']);
+        Route::post('/todos', [TodoController::class, 'create']);
+        Route::get('/todos/{id}', [TodoController::class, 'get']);
+        Route::put('/todos/{id}', [TodoController::class, 'update']);
+        Route::delete('/todos/{id}', [TodoController::class, 'delete']);
     });
 });
